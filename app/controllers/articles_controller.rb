6da1ddb2d0a61cc689b3a4c8b7ec2fcc9a4a4df1
ugochukwu <ugochukwuomeje1@gmail.com>
2020-04-29 10:42:27 +0100

    modify controller, add edit, index and show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,47 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def show
+        @article = Article.find(params[:id])
+      end
+
     def create
         @article = Article.new(article_params)
-        @article.save
+        
+        if @article.save
+
+            flash[:notice] = "article was successfully created"
+             redirect_to article_path(@article)
+        else
+          render 'new'
+        end
+        
     end
 
-    private
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    
+
+    # private
       def article_params
         params.require(:article).permit(:title, :description)
       end
+
+      def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "article was successfully updated"
+           redirect_to article_path(@article)
+        else
+           render 'edit'
+        end
+   end
+
+   def index
+      @articles = Article.all
+   end
+
+      
 end
