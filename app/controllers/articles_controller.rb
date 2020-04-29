7fe38d33bd065b564ffee66db21076617d719882
ugochukwu <ugochukwuomeje1@gmail.com>
2020-04-29 16:29:39 +0100

    extract find article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
     def new
         @article = Article.new
     end
 
     def show
-        @article = Article.find(params[:id])
+        
       end
 
     def create
@@ -22,11 +23,15 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+       
     end
 
+    def index
+        @articles = Article.all
+     end
+
     def destroy
-      @article = Article.find(params[:id])
+     
       if @article.destroy
          flash[:notice] = "Article successfuly destroyed"
          redirect_to articles_path
@@ -36,13 +41,9 @@ class ArticlesController < ApplicationController
        end
     end
 
-    # private
-      def article_params
-        params.require(:article).permit(:title, :description)
-      end
-
-      def update
-        @article = Article.find(params[:id])
+    
+    def update
+       
         if @article.update(article_params)
             flash[:notice] = "article was successfully updated"
            redirect_to article_path(@article)
@@ -50,10 +51,16 @@ class ArticlesController < ApplicationController
            render 'edit'
         end
    end
+   
+    private
+      
+      def set_article
+        @article = Article.find(params[:id])  
+      end
 
-   def index
-      @articles = Article.all
-   end
+      def article_params
+        params.require(:article).permit(:title, :description)
+      end
 
       
 end
