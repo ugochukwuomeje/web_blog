11b93e69469834ff4bcd258e81d203b0b1008bf5
ugochukwu <ugochukwuomeje1@gmail.com>
2020-05-02 18:16:11 +0100

    create user email and username validation

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  
+  validates :username, presence: true,
+            uniqueness: {case_sensitive:false},# making case_sensitive false ensures that both 
+#    apital and small letter as thesame so that you can enter thesame value irrespevtive their case
+            length: {minimum: 3, maximum: 25}
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true,
+         uniqueness: {case_sensitive: false},
+         format: { with: VALID_EMAIL_REGEX}
 end
