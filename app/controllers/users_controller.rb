65d977eb51eeb3b42698ef2e9c0fa2c8a3496a50
ugochukwu <ugochukwuomeje1@gmail.com>
2020-05-04 15:56:50 +0100

    add user update ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create
+      # debugger
      @user = User.new(user_params)
      if @user.save
         flash[:success] = "welcome to the alpha blog #{@user.username}"
@@ -13,10 +14,26 @@ class UsersController < ApplicationController
 
         render 'new'
     end
+   end
+
+   
+   def edit
+      @user = User.find(params[:id])
+   end
+
+   def update
+     @user = User.find(params[:id])
+     if @user.update(user_params)
+       flash[:success] = "Your Account was updated successfully"
+       redirect_to articles_path
+      else
+      render 'edit'
+     end
+   end
 
     private
     def user_params
         #this creates a new user object with the parameters that were passed
-       params.require(:user).permit(:usename, :email, :password)
+       params.require(:user).permit(:username, :email, :password)
     end
 end
